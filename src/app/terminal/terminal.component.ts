512854a0adc847e374036867100c26da3d5a58f8
Nathan DAUNOIS <nathandaunois@sfr.fr>
2024-04-16 19:48:23 +0200

    integrate xterm in html -> latex command will be done in python backend
    Signed-off-by: Nathan DAUNOIS <nathandaunois@sfr.fr>

--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Terminal } from '@xterm/xterm';
+// import * as child from 'child_process'; # to use in python backend!
 
 @Component({
   selector: 'app-terminal',
+  encapsulation: ViewEncapsulation.None,
   standalone: true,
   imports: [],
   templateUrl: './terminal.component.html',
-  styleUrl: './terminal.component.css'
+  styleUrls: [
+    '../../../node_modules/@xterm/xterm/css/xterm.css',
+    './terminal.component.css',
+  ],
 })
-export class TerminalComponent {
+export class TerminalComponent implements OnInit {
+  private terminal: Terminal;
 
+  constructor() {
+    this.terminal = new Terminal();
+  }
+  ngOnInit(): void {
+    const terminalElement = document.getElementById('terminal');
+    if (!terminalElement)
+      throw new Error('No Html element found for terminal.');
+    this.terminal.open(terminalElement);
+    this.terminal.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $');
+    // this.executeCommand('ls');
+  }
+
+  // executeCommand(command: string) {
+  //   const execution: child.ChildProcess = child.exec(command);
+  //   const execution_stdout = execution.stdout?.read();
+  //   this.terminal.write(execution_stdout);
+  // }
 }
