abadbbbdf51a02a25b4d6fb0dc3ed7dc669333fe
Nathan DAUNOIS <nathandaunois@sfr.fr>
2024-04-28 17:26:27 +0200

    wip*
    Signed-off-by: Nathan DAUNOIS <nathandaunois@sfr.fr>

--- install/antlr.sh
+++ install/antlr.sh
@@ -1,6 +1,9 @@
 mkdir -p builds
 cd builds
 curl -O http://www.antlr.org/download/antlr-4.13.1-complete.jar
-export CLASSPATH="./builds/antlr-4.13.1-complete.jar:$CLASSPATH"
-alias antlr4='java -Xmx500M -cp "./builds/antlr-4.13.1-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
-alias grun='java org.antlr.v4.gui.TestRig'
+export CLASSPATH="/usr/local/lib/antlr-4.13.1-complete.jar"
+echo "classpath : $CLASSPATH"
+alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.13.1-complete.jar" org.antlr.v4.Tool' || echo "ERROR"
+echo "antlr4 : $antlr4"
+alias grun='java org.antlr.v4.gui.TestRig' || echo "ERROR"
+echo "grun  : $grun"
