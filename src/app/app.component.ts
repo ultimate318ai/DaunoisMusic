ccc4aee6cb65bec315dab13edd79f88bbe54e1f5
Nathan DAUNOIS <nathandaunois@sfr.fr>
2024-05-03 15:26:11 +0200

    cleanup => json validator for json input form
    Signed-off-by: Nathan DAUNOIS <nathandaunois@sfr.fr>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { invalidJsonValidator } from './shared/Json-validator.directive';
 
 @Component({
   selector: 'app-root',
@@ -13,16 +14,26 @@ export class AppComponent {
 
   constructor() {
     this.form = new FormGroup({
-      code: new FormControl('', [Validators.required]),
+      code: new FormControl('{}', [
+        Validators.required,
+        invalidJsonValidator(),
+      ]),
     });
   }
 
-  get code(): string {
+  get code() {
     const control = this.form.get('code');
     if (!control) {
-      throw new Error("No form control for 'code'");
+      throw new Error("No control for field 'code' in form.");
     }
-    const ramValue = control.value;
-    return ramValue ?? '';
+    return control;
+  }
+
+  get codeErrors() {
+    const control = this.form.get('code');
+    if (!control) {
+      throw new Error("No control for field 'code' in form.");
+    }
+    return control?.errors?.['invalidJson']?.value;
   }
 }
--- src/app/shared/Json-validator.directive.ts
+++ src/app/shared/Json-validator.directive.ts
@@ -5,23 +5,42 @@ const mainSchema = {
   id: '/main',
   type: 'object',
   properties: {
-    name: { type: 'string' },
-    size: { type: 'string', pattern: '^(small|large|Large)$' },
+    name: { type: 'string', required: true },
+    size: { type: 'string', pattern: '^(small|large|Large)$', required: true },
   },
 };
 /** Json validator with JSON schema */
-export function invalidJsonValidator(jsonInstance: JSON): ValidatorFn {
+export function invalidJsonValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     // const extraParams = { nestedErrors: true }; TODO: use it if necessary
-    const validator = new jsonSchema.Validator();
-    const result = validator.validate(jsonInstance, mainSchema);
-    const { valid, errors } = result;
-    return !valid
-      ? {
-          invalidJson: {
-            value: errors.map((error) => error.name),
-          },
-        }
-      : null;
+
+    const ramValue = control.value;
+    try {
+      const jsonValue = JSON.parse(ramValue);
+      const validator = new jsonSchema.Validator();
+      const result = validator.validate(jsonValue, mainSchema);
+      7;
+      const { valid, errors } = result;
+      console.log(valid);
+      return !valid
+        ? {
+            invalidJson: {
+              value: errors.map((error) => {
+                console.log(error);
+                return `(${error.property}) : ${error.message}.`;
+              }),
+            },
+          }
+        : null;
+    } catch (error) {
+      if (!(error instanceof SyntaxError)) {
+        throw error;
+      }
+      return {
+        invalidJson: {
+          value: [error.message],
+        },
+      };
+    }
   };
 }
